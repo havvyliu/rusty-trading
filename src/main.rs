81f432f513f5b0803e58b2bfeb6266d3dd43e0c5
Jay Liu <havvyliu@gmail.com>
2024-03-31 11:58:50 -0700

    add cron background thread to process stuff

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ use chrono::{TimeDelta, Utc};
 use std::collections::{BinaryHeap, LinkedList};
 use std::ops::Add;
 use std::sync::{Arc, Mutex, RwLock};
-use tokio::{main, select};
+use std::thread::{self, Thread};
+use std::time::Duration;
+use tokio::main;
+use tokio_cron_scheduler::{Job, JobScheduler};
 
 #[main]
 async fn main() {
@@ -17,16 +20,31 @@ async fn main() {
     let points_queue = Arc::new(RwLock::new(LinkedList::new()));
     let start_point = Arc::new(Mutex::new(Point::blank()));
     let order_book = OrderBook::new(buy_order, sell_order, points_queue, start_point);
+
+    // add scheduler
+    let scheduler = JobScheduler::new().await.unwrap();
+
+    let job = Job::new("1/5 * * * * *", |_uuid, _l| {
+        println!("I run every 5 seconds");
+    });
+    let _ = scheduler.add(job.unwrap()).await;
+    tokio::spawn(async move { scheduler.start().await });
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/daily", get(get_daily))
         .route("/transaction", post(make_transaction))
         .route("/daily", post(get_daily))
         .with_state(order_book);
-
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    tokio::time::sleep(Duration::from_secs(100)).await;
+}
+
+async fn print_stuff() {
+    print!("Hey, printing overhere");
 }
 
 //todo: Onboard DB
@@ -58,6 +76,7 @@ async fn make_transaction(
 }
 
 async fn get_daily(State(order_book): State<OrderBook>) -> (StatusCode, Json<TimeSeries>) {
+    eprint!("Hey, printing overhere");
     let start = Utc::now();
     let end = start.add(TimeDelta::minutes(1));
     let points = order_book.points();
